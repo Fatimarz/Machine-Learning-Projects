336e3f3309bae6cfeea6de613cf6b0c3a967e0c6
datascientist1.com <std_28927@iobm.edu.pk>
2024-08-22 19:18:46 +0500

    predition pipeline

--- Student_performance_prediction/src/utils.py
+++ Student_performance_prediction/src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -14,8 +15,9 @@ def save_object(file_path, obj):
 
         os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+
     except Exception as e:
         raise CustomException(e,sys)
     
@@ -45,3 +47,12 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+        try:
+            with open (file_path, "rb") as file_obj:
+                return pickle.load(file_obj)
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+        
